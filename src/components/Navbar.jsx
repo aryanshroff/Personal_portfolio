5acc3cb43accba7168c0fb5c143bfdc9271c7055
Aryan <aryanshroff21@gmail.com>
2023-06-17 19:02:51 +0530

    added imports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,17 @@
-import React from 'react'
+
+import React, { useState } from 'react';
+import {
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedin,
+  FaFacebook,
+  FaLinkedinIn,
+} from 'react-icons/fa';
+
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import Logo from '../assets/ary1-removebg-preview.png';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
   return (
